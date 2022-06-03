479fd4d689f83adc30c1970f1436071de5647b42
Tara <taratrody@gmail.com>
2022-06-03 16:47:27 -0400

    create db instance and connect app to db

--- server.py
+++ server.py
@@ -10,3 +10,11 @@ app = Flask(__name__) # points to a flask object
 # Allows use of local file as database
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
 app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+
+db = SQLAlchemy(app)
+@event.listens_for(Engine, "connect")
+def _set_sqlite_pragma(dbapi_connection, connection_record):
+    if isinstance(dbapi_connection, SQLite3Connection):
+        cursor = dbapi_connection.cursor()  
+        cursor.execute("PRAGMA foreign_keys=ON")
+        cursor.close()
