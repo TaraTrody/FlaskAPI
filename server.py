81727e96e6424c7832f082b2ec7f5097e67f09ba
Tara <taratrody@gmail.com>
2022-06-03 16:48:43 -0400

    create models for users and blog posts

--- server.py
+++ server.py
@@ -18,3 +18,25 @@ def _set_sqlite_pragma(dbapi_connection, connection_record):
         cursor = dbapi_connection.cursor()  
         cursor.execute("PRAGMA foreign_keys=ON")
         cursor.close()
+
+
+now = datetime.now()
+# models
+class User(db.Model):
+    __tablename__ = "user"
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50))
+    name = db.Column(db.String(50))
+    address = db.Column(db.String(200))
+    phone = db.Column(db.String(50))
+    posts = db.relationship("BlogPost")
+
+class BlogPost(db.Model):
+    __tablename__ = "blog_post"
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(50))
+    body = db.Column(db.String(200))
+    date = db.Column(db.Date)   
+    # the blog post has a user Id associated and it's linked to the user Table so the id 
+    # can't be null. That's why we use the enforce foreign key configuration above
+    user_id = db.Column(db.Integer, db.ForeignKey("user.id"), nullable=False) 
