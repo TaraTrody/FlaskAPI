3613a8e520846743be805714b6e9f579071afd73
Tara <taratrody@gmail.com>
2022-06-03 16:45:53 -0400

    Initialize and configure Flask app

--- server.py
+++ server.py
@@ -1,1 +1,8 @@
 from flask import Flask, request, jsonify
+# App  
+app = Flask(__name__) # points to a flask object
+
+# Database configuration
+# Allows use of local file as database
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///sqlitedb.file"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
